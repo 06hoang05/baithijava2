c102a6d8f9316393938c2352bfc784a1c2bb72dd
06hoang05 <97800448+06hoang05@users.noreply.github.com>
2022-07-11 10:31:51 +0700

    Add files via upload

---
+++ baithi2/InterProduct.java
@@ -0,0 +1,11 @@
+package baithi2;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface InterProduct {
+    public int insert (Product product) throws SQLException;
+    public int save (Product product) throws  SQLException;
+    public Product select (int BookId) throws SQLException;
+    public List<Product> getAll() throws SQLException;
+}
